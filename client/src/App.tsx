359f2a6977d9122d67c7117fba1480975f7787d5
yochiba <yoshikazu.oshiba@gmail.com>
2021-07-03 10:43:54 +0900

    develop_2_7

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.scss';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
 import Navigation from './component/Navigation';
 import ProductList from './component/ProductList';
 import ProductDetail from './component/ProductDetail';
 import Register from './component/Register';
 import RegisterConfirm from './component/RegisterConfirm';
 import Complete from './component/Complete';
+
+library.add(faShoppingCart);
 
 const App: React.FC = () => {
   return (
--- client/src/component/Navigation.tsx
+++ client/src/component/Navigation.tsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Navigation: React.FC = () => {
   return (
     <nav className='Navigation'>
       <Link to='/'>Home</Link>
       <div className='cart'>
-        <Link to='/register'>cart: ここにカートに入っている個数を表示できるようにする。</Link>
+        <Link to='/register'>
+          <FontAwesomeIcon icon='shopping-cart' />
+        </Link>
       </div>
     </nav>
   );
